654aaf0aa6a790aed9922549d8b256aa23be8005
오세영 <leenight5309@naver.com>
2026-02-21 23:15:11 +0900

    feat: port 수정

--- server/server.js
+++ server/server.js
@@ -555,7 +555,7 @@ function endVoting(roomId) {
   });
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Liar Game Server running on port ${PORT}`);
+  console.log("Server running on", PORT);
 });
